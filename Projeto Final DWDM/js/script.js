d2d4aa3ac5ee506dd56011b74313985af071d622
Antônio Costa <antonioclaudiocostajr@gmail.com>
2024-11-19 20:57:36 -0300

    Commit Antônio Aula 19/11

--- Projeto Final DWDM/js/script.js
+++ Projeto Final DWDM/js/script.js
@@ -1,27 +1,10 @@
 $(document).ready(function() {
-    $('.gallery-slider').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000
+    $('.testimonials-slider').slick({
+        infinite: true, // Loop infinito
+        speed: 500, // Velocidade da transição
+        slidesToShow: 3, // Quantidade de slides visíveis
+        slidesToScroll: 1, // Quantidade de slides que mudam por clique
+        autoplay: true, // Habilita autoplay
+        autoplaySpeed: 4000 // Velocidade do autoplay (em ms)
     });
-    const lightbox = document.getElementById("lightbox");
-    const lightboxImage = document.getElementById("lightboxImage");
-    const captionText = document.getElementById("caption");
-    $('.grid-image').on('click', function() {
-        lightbox.style.display = "block";
-        lightboxImage.src = this.src;
-        captionText.innerHTML = this.alt;
-    });
-    $('.close').on('click', function() {
-        lightbox.style.display = "none";
-    });
-    $(window).on('click', function(event) {
-        if (event.target === lightbox) {
-            lightbox.style.display = "none";
-        }
-    });
-});
+  });
