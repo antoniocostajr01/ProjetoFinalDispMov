cffdd80f40af06c69c52799d263d6fb3e093e2f4
antoniocostajr01 <antonioclaudiocostajr@gmail.com>
2024-11-25 16:18:01 -0300

    Merge testes to main (SwiperJS)

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,25 +1,20 @@
-$(".testimonials-slider").slick({
-     centerMode: true,
-     centerPadding: '60px',
-     slidesToShow: 3,
-     responsive: [
-       {
-         breakpoint: 768,
-         settings: {
-           arrows: false,
-           centerMode: true,
-           centerPadding: '40px',
-           slidesToShow: 3
-         }
-       },
-       {
-         breakpoint: 480,
-         settings: {
-           arrows: false,
-           centerMode: true,
-           centerPadding: '40px',
-           slidesToShow: 1
-         }
-       }
-     ]
-   });
+var swiper = new Swiper(".mySwiper", {
+      autoplay: {
+            delay: 2000
+      },
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      },
+      navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+      },
+      scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+      },
+});
