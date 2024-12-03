3596b7cfa83389ea4d8a5ba268acad5fca6b05d5
antoniocostajr01 <antonioclaudiocostajr@gmail.com>
2024-12-03 15:32:26 -0300

    Alguns recursos de responsividade adicionados

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,4 +17,22 @@ var swiper = new Swiper(".mySwiper", {
             el: ".swiper-scrollbar",
             hide: true,
       },
+      breakpoints :{
+            1500: {
+                  slidesPerView: 3,
+                  spaceBetween: 30
+            },
+            1100: {
+                  slidesPerView: 2,
+                  spaceBetween: 18
+            },
+            900: {
+                  slidesPerView: 1,
+                  spaceBetween: 18
+            },
+            300:{
+                  slidesPerView: 1,
+                  spaceBetween: 18
+            }
+      }
 });
